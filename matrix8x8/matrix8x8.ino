e0fb56effad2468ce844628e12aff9fb3058ee11
Mark Vodyanitskiy <mvodya@ya.ru>
2018-06-24 00:53:20 +1000

    test example code

--- matrix8x8/matrix8x8.ino
+++ matrix8x8/matrix8x8.ino
@@ -1,9 +1,57 @@
-#define LATCH_PIN 7
-#define CLOCK_PIN 6
-#define DATA_PIN 8
+#include "matrix8.h"
+
+// Create matrix8
+//   latch - 7
+//   clock - 6
+//   data  - 8
+Matrix8 mat(7, 6, 8);
 
 void setup() {
 }
 
+unsigned int a = 0;
+int state = 0;
+int x = 0, y = 0;
+bool stab = true;
+void up() {
+  
+  mat.set(x, y, stab);
+  
+  if (state == 0)
+    x++;
+  if (state == 1)
+    y++;
+  if (state == 2)
+    x--;
+  if (state == 3)
+    y--;
+
+  
+  if (state == 0 && x == 7 - a)
+    state = 1;
+  if (state == 1 && y == 7 - a)
+    state = 2;
+  if (state == 2 && x == 0 + a)
+    state = 3;
+  if (state == 3 && y == 1 + a) {
+    state = 0;
+    a++;
+  }
+  if (a == 4) {
+    mat.set(x, y, stab);
+    a = 0;
+    x = 0;
+    y = 0;
+    state = 0;
+    stab = !stab;
+  }
+}
+
+unsigned long ltime = 0;
 void loop() {
+  if (ltime + 100000 < micros()) {
+    ltime = micros();
+    up();
+  }
+  mat.draw();
 }
