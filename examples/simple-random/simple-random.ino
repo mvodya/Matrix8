537760430fb00685b98fb039b16a18dc59ab6f1b
Mark Vodyanitskiy <voderme@gmail.com>
2018-07-05 23:50:34 +1000

    Simple random (TODO)

---
+++ examples/simple-random/simple-random.ino
@@ -0,0 +1,14 @@
+// Matrix8 library example
+// Simple random
+// by Mark Vodyanitskiy (@mvodya) 2018
+#include <matrix8.h>
+
+// Init matrix8
+//   latch - 7
+//   clock - 6
+//   data  - 8
+Matrix8 matrix(7, 6, 8);
+
+void setup() {}
+
+void loop() {}
